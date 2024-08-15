92026194e0b9e9fe5da25baac19b849b8f712934
Carlos Mejia <mejia.carlose@javeriana.edu.co>
2024-08-15 15:17:48 -0500

    infoMascota, aun faltan pequeños detalles del css

--- src/main/java/com/example/demo/repositorio/MascotaRepository.java
+++ src/main/java/com/example/demo/repositorio/MascotaRepository.java
@@ -14,15 +14,15 @@ public class MascotaRepository {
     private Map<Integer, Mascota> data = new HashMap<>();
 
     public MascotaRepository() {
-        data.put(1, new Mascota("Michito", 3, "persa", 5.3f, "gastritis", "En tratamiento", "20/09/18", "20/09/18",
+        data.put(1, new Mascota("Michito", 3, "angola", 5.3f, "", "En tratamiento", "20/09/18", "20/09/18",
                 "esomeprazol",
                 "https://images.ctfassets.net/denf86kkcx7r/4IPlg4Qazd4sFRuCUHIJ1T/f6c71da7eec727babcd554d843a528b8/gatocomuneuropeo-97?fm=webp&w=612"));
-        data.put(2, new Mascota("Bella", 4, "persa", 4.8f, "gastritis", "En tratamiento", "15/10/18", "20/09/18",
-                "esomeprazol", "https://images.ctfassets.net/denf86kkcx7r/4IPlg4Qazd4sFRuCUHIJ1T/f6c71da7eec727babcd554d843a528b8/gatocomuneuropeo-97?fm=webp&w=612"));
-        data.put(3, new Mascota("Tito", 2, "persa", 3.2f, "gastritis", "En tratamiento", "05/12/18", "20/09/18",
-                "esomeprazol", "https://images.ctfassets.net/denf86kkcx7r/4IPlg4Qazd4sFRuCUHIJ1T/f6c71da7eec727babcd554d843a528b8/gatocomuneuropeo-97?fm=webp&w=612"));
-        data.put(4, new Mascota("Lucy", 5, "persa", 3.7f, "gastritis", "En tratamiento", "10/01/19", "20/09/18",
-                "esomeprazol", "https://images.ctfassets.net/denf86kkcx7r/4IPlg4Qazd4sFRuCUHIJ1T/f6c71da7eec727babcd554d843a528b8/gatocomuneuropeo-97?fm=webp&w=612"));
+        data.put(2, new Mascota("Bella", 4, "minecoon", 4.8f, "", "En tratamiento", "15/10/18", "20/09/18",
+                "esomeprazol", "https://urgenciesveterinaries.com/wp-content/uploads/2023/09/survet-gato-caida-pelo-01.jpeg"));
+        data.put(3, new Mascota("Tito", 2, "angola", 3.2f, "", "Tratado", "05/12/18", "20/09/18",
+                "esomeprazol", "https://fotografias.lasexta.com/clipping/cmsimages02/2019/01/25/DB41B993-B4C4-4E95-8B01-C445B8544E8E/98.jpg?crop=4156,2338,x0,y219&width=1900&height=1069&optimize=high&format=webply"));
+        data.put(4, new Mascota("Lucy", 5, "persa", 3.7f, "", "En tratamiento", "10/01/19", "20/09/18",
+                "esomeprazol", "https://www.lapulgada.co/cdn/shop/collections/gato-persa-2_675x675.png?v=1721917322"));
     }
 
     public Mascota getMascotaById(int id) {
