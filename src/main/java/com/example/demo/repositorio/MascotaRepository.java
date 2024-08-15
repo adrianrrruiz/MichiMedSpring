c5feb5e69ee5034b5f7db8113a064cf5903efe8e
Carlos Mejia <mejia.carlose@javeriana.edu.co>
2024-08-15 13:56:05 -0500

    Mascotas y estilos, falta infoMascota

--- src/main/java/com/example/demo/repositorio/MascotaRepository.java
+++ src/main/java/com/example/demo/repositorio/MascotaRepository.java
@@ -18,11 +18,11 @@ public class MascotaRepository {
                 "esomeprazol",
                 "https://images.ctfassets.net/denf86kkcx7r/4IPlg4Qazd4sFRuCUHIJ1T/f6c71da7eec727babcd554d843a528b8/gatocomuneuropeo-97?fm=webp&w=612"));
         data.put(2, new Mascota("Bella", 4, "persa", 4.8f, "gastritis", "En tratamiento", "15/10/18", "20/09/18",
-                "esomeprazol", "dksanbhjda"));
+                "esomeprazol", "https://images.ctfassets.net/denf86kkcx7r/4IPlg4Qazd4sFRuCUHIJ1T/f6c71da7eec727babcd554d843a528b8/gatocomuneuropeo-97?fm=webp&w=612"));
         data.put(3, new Mascota("Tito", 2, "persa", 3.2f, "gastritis", "En tratamiento", "05/12/18", "20/09/18",
-                "esomeprazol", "dksanbhjda"));
+                "esomeprazol", "https://images.ctfassets.net/denf86kkcx7r/4IPlg4Qazd4sFRuCUHIJ1T/f6c71da7eec727babcd554d843a528b8/gatocomuneuropeo-97?fm=webp&w=612"));
         data.put(4, new Mascota("Lucy", 5, "persa", 3.7f, "gastritis", "En tratamiento", "10/01/19", "20/09/18",
-                "esomeprazol", "dksanbhjda"));
+                "esomeprazol", "https://images.ctfassets.net/denf86kkcx7r/4IPlg4Qazd4sFRuCUHIJ1T/f6c71da7eec727babcd554d843a528b8/gatocomuneuropeo-97?fm=webp&w=612"));
     }
 
     public Mascota getMascotaById(int id) {
