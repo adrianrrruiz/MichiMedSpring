2accea5cb6b31e672e20c3f1506211847da41d8d
Adrian Ruiz <adrianrrruiz@gmail.com>
2024-08-16 17:29:07 -0500

    Advance on "Sign In" screen

--- src/main/java/com/example/demo/controlador/SignInController.java
+++ src/main/java/com/example/demo/controlador/SignInController.java
@@ -1,17 +1,18 @@
 package com.example.demo.controlador;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// http://localhost:8090/
-@RequestMapping("/")
+// http://localhost:8090/sign-in
+@RequestMapping("/sign-in")
 @Controller
-public class LandingController {
-    
-    //http://localhost:8090/
+public class SignInController {
+
+    // http://localhost:8090/sign-in
     @GetMapping
-    public String landing() {
-        return "index";
+    public String SignIn(Model model) {
+        return "signIn";
     }
 }
