c33b510d024719b68a42b534f6cc272a734bffc5
Carlos Mejia <mejia.carlose@javeriana.edu.co>
2024-10-01 14:44:44 -0500

    REST clientes y sign in

--- src/main/java/com/example/demo/controlador/ClienteController.java
+++ src/main/java/com/example/demo/controlador/ClienteController.java
@@ -1,77 +1,56 @@
 package com.example.demo.controlador;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entidad.Cliente;
 import com.example.demo.servicio.ClienteService;
 
 @RequestMapping("/clientes")
-@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ClienteController {
-
     String ruta = "clientes/";
-    
+
     @Autowired
     ClienteService clienteService;
 
+    // http://localhost:8090/clientes
     @GetMapping("")
-    public String Clientes(Model model) {
-        model.addAttribute("clientes", clienteService.SearchAll());
-        return ruta + "clientes";
+    public Collection<Cliente> getAllClientes() {
+        return clienteService.SearchAll();
     }
 
-    @GetMapping("/update/{id}")
-    public String mostrarFormularioUpdate(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("cliente", clienteService.SearchById(id));
-        return ruta + "updateCliente";
+    // http://localhost:8090/clientes/{id}
+    @GetMapping("/{id}")
+    public Cliente getClienteById(@PathVariable("id") Long id) {
+        return clienteService.SearchById(id);
     }
 
-    @PostMapping("/update/{id}")
-    public String updateCliente(@PathVariable("id") Long id, @ModelAttribute Cliente cliente, Model model) {
-        try {
-            clienteService.update(cliente);
-            return "redirect:/clientes";
-        } catch (IllegalArgumentException e) {
-            model.addAttribute("error", e.getMessage());
-            return ruta + "updateCliente";
-        } catch (DataIntegrityViolationException e) {
-            model.addAttribute("error", "Ya existe un cliente con este correo electrónico.");
-            return ruta + "updateCliente";
-        }
-    }
-
-    @GetMapping("/delete/{id}")
-    public String deleteCliente(@PathVariable("id") Long id) {
-        clienteService.deleteById(id);
-        return "redirect:/clientes";
-    }
-
-    @GetMapping("/mascotas/{id}")
-    public String findMascotasCliente(Model model, @PathVariable("id") Long id, @RequestParam(required = false) String nombre) {
-        model.addAttribute("mascotas", clienteService.getMascotas(id));
-        if (nombre != null) {
-            model.addAttribute("nombreCliente", nombre);
-            return "mascotas/mascotasCliente";
-        }
-        return "clientes/mascotasCliente";
-    }
-
+    // http://localhost:8090/clientes/add
     @GetMapping("/add")
     public String mostrarFormularioCrear(Model model) {
-        Cliente cliente = new Cliente(null, null, null, null);
+        Cliente cliente = new Cliente();
         model.addAttribute("cliente", cliente);
         return ruta + "clientesAdd";
     }
 
+    // http://localhost:8090/clientes/add
     @PostMapping("/add")
     public String addCliente(@ModelAttribute("cliente") Cliente cliente, Model model) {
         try {
@@ -81,14 +60,27 @@ public class ClienteController {
             model.addAttribute("error", e.getMessage());
             return ruta + "clientesAdd";
         } catch (DataIntegrityViolationException e) {
-            model.addAttribute("error", "Ya existe un cliente con este correo electrónico.");
+            model.addAttribute("error", "Ya existe un cliente con esta cédula.");
             return ruta + "clientesAdd";
         }
     }
 
-    @GetMapping("/{id}")
-    public String InfoCliente(Model model, @PathVariable("id") Long id) {
+    // http://localhost:8090/clientes/update/{id}
+    @GetMapping("/update/{id}")
+    public String mostrarFormularioUpdate(@PathVariable("id") Long id, Model model) {
         model.addAttribute("cliente", clienteService.SearchById(id));
-        return ruta + "infoClientes";
+        return ruta + "updateCliente";
+    }
+
+    // http://localhost:8090/clientes/update
+    @PutMapping("/update")
+    public void updateCliente(@RequestBody Cliente cliente) {
+        clienteService.update(cliente);
+    }
+
+    // http://localhost:8090/clientes/delete/{id}
+    @DeleteMapping("/delete/{id}")
+    public void deleteCliente(@PathVariable("id") Long id) {
+        clienteService.deleteById(id);
     }
 }
--- src/main/java/com/example/demo/controlador/SignInController.java
+++ src/main/java/com/example/demo/controlador/SignInController.java
@@ -1,45 +1,31 @@
 package com.example.demo.controlador;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.entidad.User;
 import com.example.demo.servicio.ClienteServiceInterface;
 
-
-// http://localhost:8090/sign-in
+@RestController
 @RequestMapping("/sign-in")
-@Controller
 public class SignInController {
 
     @Autowired
     ClienteServiceInterface clienteService;
 
-    // http://localhost:8090/sign-in
     @GetMapping
-    public String SignIn(Model model) {
+    public ResponseEntity<User> signIn() {
         User user = new User("", "");
-
-        model.addAttribute("user", user);
-        model.addAttribute("error", "");
-
-        return "signIn";
+        return ResponseEntity.ok(user);
     }
 
-    // http://localhost:8090/sign-in
     @PostMapping
-    public String verifyCredentials(@ModelAttribute("user") User user, Model model) {
+    public ResponseEntity<?> verifyCredentials(@RequestBody User user) {
         Long id = clienteService.verifyCredentials(user);
-        if(id != -1L) {
-            return "redirect:/clientes/mascotas/" + id;
+        if (id != -1L) {
+            return ResponseEntity.ok("redirect:/clientes/mascotas/" + id);
         }
-        model.addAttribute("error", "Usuario o contraseña incorrectos");
-        return "signIn";
+        return ResponseEntity.status(401).body("Usuario o contraseña incorrectos");
     }
-    
 }
