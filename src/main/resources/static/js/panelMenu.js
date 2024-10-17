7ad2a81affa5e3cbed74bc7380c8819efdc944ec
Adrian Ruiz <adrianruiz0208@gmail.com>
2024-10-17 18:11:13 -0500

    Removing innecesary files

--- src/main/resources/static/js/panelMenu.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('toggleSidebar').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.toggle('collapsed');
-    document.getElementById('content').classList.toggle('collapsed');
-});
