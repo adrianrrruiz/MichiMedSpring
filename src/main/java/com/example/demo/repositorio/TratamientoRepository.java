b72b7569352b1841800ac52176e5f6922a9631c1
Carlos Mejia <mejia.carlose@javeriana.edu.co>
2024-10-17 15:35:36 -0500

    tratamientos

--- src/main/java/com/example/demo/repositorio/TratamientoRepository.java
+++ src/main/java/com/example/demo/repositorio/TratamientoRepository.java
@@ -14,6 +14,7 @@ public interface TratamientoRepository extends JpaRepository<Tratamiento, Long> 
     @Query(value = "SELECT FORMATDATETIME(PARSEDATETIME(fecha, 'dd/MM/yy'), 'MMMM yyyy', 'es') AS mes, COUNT(*) AS cantidad FROM TRATAMIENTO GROUP BY FORMATDATETIME(PARSEDATETIME(fecha, 'dd/MM/yy'), 'MMMM yyyy', 'es') ORDER BY MIN(PARSEDATETIME(fecha, 'dd/MM/yy'));", nativeQuery = true)
     List<Map<String, Object>> contarTratamientosPorMes();
 
-    @Query(value = "SELECT v.NOMBRE AS veterinario_nombre, d.NOMBRE AS droga_nombre, SUM(d.UNI_VEND) AS total_unidades_vendidas FROM TRATAMIENTO t JOIN VETERINARIO v ON t.VETERINARIO_ID = v.id JOIN DROGA d ON t.DROGA_ID = d.id GROUP BY v.NOMBRE, d.NOMBRE ORDER BY total_unidades_vendidas DESC LIMIT 3", nativeQuery = true)
+    @Query(value = "SELECT v.NOMBRE AS veterinario_nombre,  d.NOMBRE AS droga_nombre,  SUM(d.UNI_VEND) AS total_unidades_vendidas FROM TRATAMIENTO t JOIN VETERINARIO v ON t.VETERINARIO_ID = v.ID JOIN DROGA d ON t.DROGA_ID = d.ID GROUP BY t.ID, v.NOMBRE, d.NOMBRE ORDER BY total_unidades_vendidas DESC LIMIT 3;\n" + //
+                "", nativeQuery = true)
     List<Map<String, Object>> findTopVeterinariosByDroga();
 }
