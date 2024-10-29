d8ab6c3c85f1b91f87610874d0d70a8c670e2967
Adrian Ruiz <adrianruiz0208@gmail.com>
2024-10-29 16:30:21 -0500

    Advance UseCase1

--- src/test/java/com/example/demo/e2e/UseCase1Test.java
+++ src/test/java/com/example/demo/e2e/UseCase1Test.java
@@ -2,9 +2,12 @@ package com.example.demo.e2e;
 
 import java.time.Duration;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,7 +20,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class Case1Test {
+public class UseCase1Test {
+
+  private final String BASE_URL = "http://localhost:4200";
 
   private WebDriver driver;
   private WebDriverWait wait; // Espera a que ocurra algo en la página para hacer la prueba
@@ -29,16 +34,39 @@ public class Case1Test {
 
     ChromeOptions options = new ChromeOptions();
 
-    options.addArguments("--disable-gpu", "--window-size=1920,1200",
-        "--ignore-certificate-errors");
+    options.addArguments("--disable-notifications");
+    options.addArguments("--disable-extensions");
 
     this.driver = new ChromeDriver(options);
-    this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));// Para que espere máximo 5 segundos
 
   }
 
   @Test
   public void SystemTest_Case1_Completed() {
-    driver.get("http://localhost:4200/sign-in");
+    driver.get(BASE_URL + "/sign-in");
+
+    WebElement inputCedula = driver.findElement(By.id("cedula"));
+    WebElement inputContrasena = driver.findElement(By.id("password"));
+
+    inputCedula.sendKeys("987654");
+    inputContrasena.sendKeys("4321");
+
+    WebElement btnIngresar = driver.findElement(By.id("btnIngresar"));
+    btnIngresar.click();
+
+    inputCedula.clear();
+    inputContrasena.clear();
+
+    inputCedula.sendKeys("987654321");
+    inputContrasena.sendKeys("4321");
+
+    btnIngresar.click();
+
+  }
+
+  @AfterEach
+  void tearDown() {
+    // driver.quit();
   }
 }
