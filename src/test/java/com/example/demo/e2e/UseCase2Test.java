c83a62a9c55a32961141df8da4182e6e3a086dac
Carlos Mejia <mejia.carlose@javeriana.edu.co>
2024-10-30 23:31:28 -0500

    Add e2e test for UseCase2

--- src/test/java/com/example/demo/e2e/UseCase2Test.java
+++ src/test/java/com/example/demo/e2e/UseCase2Test.java
@@ -6,10 +6,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -44,7 +47,7 @@ public class UseCase2Test {
   }
 
   @Test
-  public void SystemTest_Case1_Completed() {
+  public void SystemTest_Case2_Completed() {
     driver.get(BASE_URL + "/sign-in");
 
     wait.until(ExpectedConditions.presenceOfElementLocated(By.id("cedula")));
@@ -53,7 +56,7 @@ public class UseCase2Test {
     WebElement inputCedula = driver.findElement(By.id("cedula"));
     WebElement inputContrasena = driver.findElement(By.id("password"));
 
-    inputCedula.sendKeys("123456789");
+    inputCedula.sendKeys("1052380081");
     inputContrasena.sendKeys("1234");
 
     wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnIngresar")));
@@ -61,17 +64,115 @@ public class UseCase2Test {
     WebElement btnIngresar = driver.findElement(By.id("btnIngresar"));
     btnIngresar.click();
 
-    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("tratamientoNavLink")));
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("dashboardNavLink")));
 
-    WebElement tratamientoNavLink = driver.findElement(By.id("tratamientoNavLink"));
+    WebElement tratamientoNavLink = driver.findElement(By.id("dashboardNavLink"));
     tratamientoNavLink.click();
 
+    // Realiza un desplazamiento hacia abajo
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    js.executeScript("window.scrollBy(0,550)", "");
+
+    // Espera de 3 segundos
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    // Vuelve a localizar los elementos después del desplazamiento
+    WebElement logOutNavLink = driver.findElement(By.id("logOutNavLink"));
+    logOutNavLink.click();
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("logInButton")));
+    WebElement logInButton = driver.findElement(By.id("logInButton"));
+    logInButton.click();
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("cedula")));
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("password")));
+
+    inputCedula = driver.findElement(By.id("cedula"));
+    inputContrasena = driver.findElement(By.id("password"));
+
+    inputCedula.sendKeys("123456789");
+    inputContrasena.sendKeys("1234");
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnIngresar")));
+
+    btnIngresar = driver.findElement(By.id("btnIngresar"));
+    btnIngresar.click();
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("tratamientoNavLink")));
+
+    tratamientoNavLink = driver.findElement(By.id("tratamientoNavLink"));
+    tratamientoNavLink.click();
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"mascotas\"]/div")));
+    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"mascotas\"]/div")));
+
+    WebElement inputMascotas = driver.findElement(By.xpath("//*[@id=\"mascotas\"]/div"));
+    inputMascotas.click();
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"mascotas_list\"]/p-dropdownitem[1]")));
+    WebElement dropdownItem = driver.findElement(By.xpath("//*[@id=\"mascotas_list\"]/p-dropdownitem[1]"));
+    dropdownItem.click();
+
+    WebElement inputDrogas = driver.findElement(By.xpath("//*[@id=\"pn_id_3\"]/div"));
+    inputDrogas.click();
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"pn_id_3_list\"]/p-dropdownitem[1]")));
+    WebElement dropdownItemDrogas = driver.findElement(By.xpath("//*[@id=\"pn_id_3_list\"]/p-dropdownitem[1]"));
+    dropdownItemDrogas.click();
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnSaveTratamiento")));
+    WebElement btnSaveTratamiento = driver.findElement(By.id("btnSaveTratamiento"));
+    btnSaveTratamiento.click();
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("historialNavLink")));
+    logInButton = driver.findElement(By.id("historialNavLink"));
+    logInButton.click();
+
     wait.until(ExpectedConditions.presenceOfElementLocated(By.id("mascotas")));
+    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"mascotas\"]/div")));
+    inputMascotas = driver.findElement(By.xpath("//*[@id=\"mascotas\"]/div"));
+    inputMascotas.click();
 
-    WebElement mascotas = driver.findElement(By.id("mascotas"));
-    mascotas.click();
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"mascotas_list\"]/p-dropdownitem[1]")));
+    dropdownItem = driver.findElement(By.xpath("//*[@id=\"mascotas_list\"]/p-dropdownitem[1]"));
+    dropdownItem.click();
 
-    
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnSaveHistorial")));
+    btnSaveTratamiento = driver.findElement(By.id("btnSaveHistorial"));
+    btnSaveTratamiento.click();
+
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("logOutNavLink")));
+    logOutNavLink = driver.findElement(By.id("logOutNavLink"));
+    logOutNavLink.click();
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("logInButton")));
+    logInButton = driver.findElement(By.id("logInButton"));
+    logInButton.click();
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("cedula")));
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("password")));
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnIngresar")));
+
+    inputCedula = driver.findElement(By.id("cedula"));
+    inputContrasena = driver.findElement(By.id("password"));
+    inputCedula.sendKeys("1052380081");
+    inputContrasena.sendKeys("1234");
+    logInButton = driver.findElement(By.id("btnIngresar"));
+    logInButton.click();
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"content\"]/div[3]/app-profit/div/div[1]/div/div[2]/p")));
+    js.executeScript("window.scrollBy(0,550)", "");
+
 
 
   }
